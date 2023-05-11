456760d48cf4fcd720c15ba8ee45b01abea95f6f
ImDaria108 <daria.immerman@gmail.com>
2023-05-12 00:32:27 +0300

    Initial commit

---
+++ src/main/java/config/AppiumConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AppiumConfig {
+
+    //{
+        //"platformName": "Android",
+           // "deviceName": "Daria",
+            //"platformVersion": "8.0",
+           // "appPackage": "com.sheygam.contactapp",
+            //"appActivity": ".SplashActivity"
+   // }
+
+    protected static AppiumDriver<AndroidElement> driver;
+
+
+
+
+}
